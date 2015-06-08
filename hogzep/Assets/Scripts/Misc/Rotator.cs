7d7cd3ca6aeb809dbbef75359f8c8a0dab060914
hogzep <hogzep@gmail.com>
2015-06-08 14:00:49 +0300

    init

---
+++ hogzep/Assets/Scripts/Misc/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Rotator : MonoBehaviour 
+{
+	void Start () 
+    {
+        this.transform.DOMoveY(2, 2).SetRelative().SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        this.transform.DORotate(new Vector3(0, 360, 0), 6, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+
+	}
+}
---
+++ hogzep/Assets/Scripts/Misc/Tweaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Tweaker : MonoBehaviour
+{
+    public int frameRateLimit = 120;
+    public bool limitFrameRate;
+    public bool hideCursor;
+    public bool lockCursor;
+
+    void Start()
+    {
+        if (limitFrameRate == true)
+        {
+            Application.targetFrameRate = frameRateLimit;
+            Debug.Log("FPS limit is set to: " + frameRateLimit);
+        }
+        else { Debug.Log("FPS limit is disabled."); }
+
+        if (hideCursor == true)
+        {
+            Cursor.visible = false;
+        }
+
+        if (lockCursor == true)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
